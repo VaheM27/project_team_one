42d2ddf1e8da95c0b14dbbaa01be59232ec9ca5e
Manukyan Albert <albertmanukyan2007@gmail.com>
2024-10-10 23:15:45 +0400

    Sign In Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/feature/Navbar/Navbar";
 
-import "./App.css";
+import "./App.scss";
+import Signin from "./components/feature/Sign_in/Signin";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Signin />
     </div>
   );
 }
