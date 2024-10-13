47410979686410d8f3837aa073cca167506a58b3
Manukyan Albert <albertmanukyan2007@gmail.com>
2024-10-13 17:12:21 +0400

    Sign in Component changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,20 @@ import Signin from "./components/feature/Sign_in/Signin";
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
       <Signin />
+      {/* <Router>
+        <Routes>
+          <Route path="/support-center" element={<SupportCenter />} />
+          <Route path="/invoicing" element={<Invoicing />} />
+          <Route path="/contract" element={<Contract />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+        <Footer />
+      </Router> */}
     </div>
   );
 }
