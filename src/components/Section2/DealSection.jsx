c041977b350f12ded83b191c3bb266f2a37fd89f
Vardazarryan <vardazaryanvarduhi99@gmail.com>
2024-10-15 21:04:37 +0400

    updateSliderandTimer

--- src/components/Section2/DealSection.jsx
+++ src/components/Section2/DealSection.jsx
@@ -1,38 +1,35 @@
-import React from "react";
-import Slider from "../Section2/Slider";
+import React from 'react';
+import Slider from '../Section2/Slider';
+import Timer from '../Section2/Timer'; // Ensure this is the correct path
+import styles from './DealSection.module.scss';
 
-import "./DealSection.scss";
-
-import image1 from "../../assets/images/images (2).png";
-import image2 from "../../assets/images/image1.png";
-import image3 from "../../assets/images/image2.png";
+import image1 from '../../assets/images/images (2).png';
+import image2 from '../../assets/images/image1.png';
+import image3 from '../../assets/images/image2.png';
 
 const images = [image1, image2, image3];
 
-const DealsSection = () => {
+const DealSection = () => {
   return (
-    <div className="deals-section">
-      <div className="deals-left">
-        <h2 className="deals-title">Deals Of The Month</h2>
-        <p className="deals-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque
-          duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices
-          sollicitudin
+   <div className={styles.container}>
+     <div className={styles.dealsSection}>
+      <div className={styles.dealsLeft}>
+        <h2 className={styles.dealsTitle}>Deals Of The Month</h2>
+        <p className={styles.dealsDescription}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin
         </p>
-        <button className="buy-now-btn">Buy Now</button>
-        <p className="hurry-text">Hurry, Before It's Too Late!</p>
-        <div className="timer">
-          <span>Days: 10</span>
-          <span>Hours: 5</span>
-          <span>Minutes: 30</span>
-          <span>Seconds: 45</span>
+        <button className={styles.buyNowBtn}>Buy Now</button>
+        <p className={styles.hurryText}>Hurry, Before It's Too Late!</p>
+        <div className={styles.timer}>
+          <Timer /> {/* Include the Timer component here */}
         </div>
       </div>
-      <div className="deals-right">
+      <div className={styles.dealsRight}>
         <Slider images={images} />
       </div>
     </div>
+   </div>
   );
 };
 
-export default DealsSection;
+export default DealSection;
