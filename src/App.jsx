05a202244e249c029f5aa64bfa3bafe824f05f91
Gor-492 <goraydinyan07@gmail.com>
2024-10-19 18:20:53 +0400

    create recycleBin in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ import Error404 from "../src/Pages/Error404";
 import Navbar from "./components/feature/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
+import DealSection from "./components/Section2/DealSection";
+import Newarrivals from "./components/feature/Newarrivals/Newarrivals";
+import Logos from "./components/feature/Logos/Logos";
+import Peaky from "./components/feature/Peaky/Peaky";
+import Costumersays from "./components/feature/Costumersays/Costumersays";
+import Features from "./components/feature/Features/Features";
+import FollowUs from "./components/feature/FollowUs/FollowUs";
 
 import "./App.scss";
 
@@ -18,8 +25,13 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-
-
+      <Logos />
+      <DealSection />
+      <Newarrivals />
+      <Costumersays />
+      <Peaky />
+      <Features />
+      <FollowUs />
       <Router>
         <Routes>
           {/* <Route path="/" element={<Navbar/>} /> */}
