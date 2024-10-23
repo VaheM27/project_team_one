6a57391101f7e154dfc2cdd08ff83f88e2308c1d
Gor-492 <goraydinyan07@gmail.com>
2024-10-23 12:03:15 +0400

    fixed App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-
-import "./App.scss";
 import { Route, Routes, useLocation } from "react-router-dom";
 import ROUTES from "./Routes";
 import Layout from "./Layout/Layout";
@@ -10,12 +8,20 @@ import Signin from "./Pages/Sign_in/Signin";
 import Signup from "./Pages/Sign_up/Signup";
 import ShopComponent from "./Pages/Shop/ShopComponent";
 
+import "./App.scss";
 
 const App = () => {
-  
+
   return (
     <div className="App">
-
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={ROUTES.SIGN_IN} element={<Signin />} />
+          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+          <Route path={ROUTES.SHOP} element={<ShopComponent />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
