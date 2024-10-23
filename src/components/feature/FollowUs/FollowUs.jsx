43aa47e1de59ceede4a06ec647931f5c8a2c771c
Gor-492 <goraydinyan07@gmail.com>
2024-10-23 13:55:27 +0400

    fixed followUs

--- src/components/feature/FollowUs/FollowUs.jsx
+++ src/components/feature/FollowUs/FollowUs.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
+import FollowUsData from './FollowUsData'
 import styles from "./FollowUs.module.scss"
 import "./FollowUs.module.scss"
-import FollowUsData from './FollowUsData'
 
 const FollowUs = () => {
     return (
