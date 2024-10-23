752cea634018278c775b86b0a262c89b982699e4
Gor-492 <goraydinyan07@gmail.com>
2024-10-23 11:22:45 +0400

    fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,6 @@ import Error404 from "../src/Pages/Error404";
 import Navbar from "./components/feature/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
-import DealSection from "./components/Section2/DealSection";
-import Newarrivals from "./components/feature/Newarrivals/Newarrivals";
-import Logos from "./components/feature/Logos/Logos";
-import Peaky from "./components/feature/Peaky/Peaky";
-import Costumersays from "./components/feature/Costumersays/Costumersays";
-import Features from "./components/feature/Features/Features";
-import FollowUs from "./components/feature/FollowUs/FollowUs";
 
 import LastSec from "./components/LastSec/LastSec";
 import DealSection from "./components/Section2/DealSection";
@@ -27,7 +20,7 @@ import Peaky from "./components/feature/Peaky/Peaky";
 import Costumersays from "./components/feature/Costumersays/Costumersays";
 import Features from "./components/feature/Features/Features";
 import FollowUs from "./components/feature/FollowUs/FollowUs";
-import { data, heading, subheading } from "./Constants/Data"; 
+import { data, heading, subheading } from "./Constants/Data";
 
 
 import "./App.scss";
@@ -39,8 +32,10 @@ const App = () => {
       <Logos />
       <DealSection />
       <Newarrivals />
-
+      <Peaky />
+      <Features />
       <FollowUs />
+      <Costumersays />
       <Router>
         <Routes>
           {/* <Route path="/" element={<Navbar/>} /> */}
